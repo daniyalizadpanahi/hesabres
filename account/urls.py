1ab595131be6903ce7ba084eacda2fdba5c8ffa7
Daniyal <daniyal.izadpanahi@gmail.com>
2025-04-12 15:46:36 +0330

    create needy and org apis

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import login_view
-
+from .views import login_view, NeedyDonationAmountView, home, needy_menu, org_menu, OrgDonationAmountView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', home, name='home'),
+    path('needy', needy_menu, name='needy_menu'),
+    path('organization', org_menu, name='org_menu'),
+    path('api/needy-donation-amount/', NeedyDonationAmountView.as_view(), name='needy-donation-amount'),
+    path('api/org-donation-amount/', OrgDonationAmountView.as_view(), name='org-donation-amount'),
     path('login/', login_view, name='login'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,50 +1,45 @@
-# # views.py
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# # from .models import Loan, Fund
-# from django.contrib.auth.models import User
-# from datetime import date
+# views.py login page
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# @api_view(['POST'])
-# def apply_for_loan(request):
-#     user = User.objects.get(id=request.data['user_id'])
-#     amount = request.data['amount']
-    
-#     # فرض بر اینکه کاربر تایید شده است
-#     if not user.userprofile.is_verified:
-#         return Response({"error": "User not verified"}, status=400)
+def login_view(request):
+    error = None
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = "نام کاربری یا رمز عبور اشتباه است."
+    return render(request, 'login.html', {'error': error})
 
-#     loan = Loan.objects.create(user=user, amount=amount, issued_date=date.today())
-#     loan.save()
+# --------------------------------------------
+from django.db.models import Sum
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CustomUser, Transaction, Inventory, Loan, Account
 
-#     # آپدیت موجودی صندوق
-#     fund = Fund.objects.first()
-#     fund.update_balance()
+def home(request):
+    return render(request, 'index.html')
 
-#     return Response({"message": "Loan applied successfully!", "loan_id": loan.id}, status=201)
+def needy_menu(request):
+    return render(request, 'needy/menu.html')
 
-# @api_view(['GET'])
-# def fund_summary(request):
-#     fund = Fund.objects.first()
-#     return Response({
-#         "total_balance": fund.total_balance,
-#         "total_debt": fund.total_debt,
-#         "net_balance": fund.total_balance - fund.total_debt
-#     })
+def org_menu(request):
+    return render(request, 'organization/menu.html')
 
+class NeedyDonationAmountView(APIView):
+    def get(self, request):
+        total_needy_donations = Inventory.objects.filter(inventory_type=Inventory.InventoryType.NEEDY_DONATION).aggregate(total_amount=Sum('amount'))['total_amount']
+        if total_needy_donations is None:
+            total_needy_donations = 0  # Return 0 if no needy donations are
+        return Response({'total_needy_donations': total_needy_donations}, status=status.HTTP_200_OK)
 
-# # views.py login page
-# from django.contrib.auth import authenticate, login
-# from django.shortcuts import render, redirect
-
-# def login_view(request):
-#     error = None
-#     if request.method == 'POST':
-#         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-#         if user:
-#             login(request, user)
-#             return redirect('home')
-#         else:
-#             error = "نام کاربری یا رمز عبور اشتباه است."
-#     return render(request, 'login.html', {'error': error})
-
+class OrgDonationAmountView(APIView):
+    def get(self, request):
+        total_org_donations = Inventory.objects.filter(inventory_type=Inventory.InventoryType.ORG_FUNDS).aggregate(total_amount=Sum('amount'))['total_amount']
+        if total_org_donations is None:
+            total_org_donations = 0  # Return 0 if no org donations are
+        return Response({'total_org_donations': total_org_donations}, status=status.HTTP_200_OK)
