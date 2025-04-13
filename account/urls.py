f472ce54748883de876b4e976bc18dcbe0a36f27
Daniyal <daniyal.izadpanahi@gmail.com>
2025-04-13 13:02:42 +0330

    create needy and org api

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,36 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from .views import login_view, NeedyDonationAmountView, home, needy_menu, org_menu, OrgDonationAmountView
+from .views import (
+    login_view,
+    NeedyDonationAmountView,
+    needy_payment,
+    home,
+    needy_menu,
+    org_payment,
+    org_menu,
+    OrgDonationAmountView,
+    DepositWithdrawAPI,
+)
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('needy', needy_menu, name='needy_menu'),
-    path('organization', org_menu, name='org_menu'),
-    path('api/needy-donation-amount/', NeedyDonationAmountView.as_view(), name='needy-donation-amount'),
-    path('api/org-donation-amount/', OrgDonationAmountView.as_view(), name='org-donation-amount'),
-    path('login/', login_view, name='login'),
-
+    path("", home, name="home"),
+    path("needy", needy_menu, name="needy_menu"),
+    path("needy/payment", needy_payment, name="needy_payment"),
+    path("organization", org_menu, name="org_menu"),
+    path("organization/payment", org_payment, name="org_payment"),
+    path(
+        "api/needy-donation-amount/",
+        NeedyDonationAmountView.as_view(),
+        name="needy-donation-amount",
+    ),
+    path(
+        "api/org-donation-amount/",
+        OrgDonationAmountView.as_view(),
+        name="org-donation-amount",
+    ),
+    path(
+        "api/inventory/<str:action>/",
+        DepositWithdrawAPI.as_view(),
+        name="deposit_withdraw",
+    ),
+    path("login/", login_view, name="login"),
 ]
--- account/views.py
+++ account/views.py
@@ -2,44 +2,107 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
+
 def login_view(request):
     error = None
-    if request.method == 'POST':
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+    if request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
         if user:
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
             error = "نام کاربری یا رمز عبور اشتباه است."
-    return render(request, 'login.html', {'error': error})
+    return render(request, "login.html", {"error": error})
+
 
 # --------------------------------------------
 from django.db.models import Sum
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import CustomUser, Transaction, Inventory, Loan, Account
+from .serializers import CreateInventorySerializer
+
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def needy_menu(request):
-    return render(request, 'needy/menu.html')
+    return render(request, "needy/menu.html")
+
 
 def org_menu(request):
-    return render(request, 'organization/menu.html')
+    return render(request, "organization/menu.html")
+
+
+def needy_payment(request):
+    return render(request, "needy/payment.html")
+
+
+def org_payment(request):
+    return render(request, "organization/payment.html")
+
 
 class NeedyDonationAmountView(APIView):
     def get(self, request):
-        total_needy_donations = Inventory.objects.filter(inventory_type=Inventory.InventoryType.NEEDY_DONATION).aggregate(total_amount=Sum('amount'))['total_amount']
+        total_needy_donations = Inventory.objects.filter(
+            inventory_type=Inventory.InventoryType.NEEDY_DONATION
+        ).aggregate(total_amount=Sum("amount"))["total_amount"]
         if total_needy_donations is None:
-            total_needy_donations = 0  # Return 0 if no needy donations are
-        return Response({'total_needy_donations': total_needy_donations}, status=status.HTTP_200_OK)
+            total_needy_donations = 0
+        return Response(
+            {"total_needy_donations": total_needy_donations}, status=status.HTTP_200_OK
+        )
+
 
 class OrgDonationAmountView(APIView):
     def get(self, request):
-        total_org_donations = Inventory.objects.filter(inventory_type=Inventory.InventoryType.ORG_FUNDS).aggregate(total_amount=Sum('amount'))['total_amount']
+        total_org_donations = Inventory.objects.filter(
+            inventory_type=Inventory.InventoryType.MOSQUE_DONATION
+        ).aggregate(total_amount=Sum("amount"))["total_amount"]
         if total_org_donations is None:
-            total_org_donations = 0  # Return 0 if no org donations are
-        return Response({'total_org_donations': total_org_donations}, status=status.HTTP_200_OK)
+            total_org_donations = 0
+        return Response(
+            {"total_org_donations": total_org_donations}, status=status.HTTP_200_OK
+        )
+
+
+class DepositWithdrawAPI(APIView):
+    def post(self, request, action):
+        inventory_type = request.data.get("inventory_type")
+        amount = request.data.get("amount")
+        account_id = request.data.get("account", 1)
+        description = request.data.get("description", "")
+
+        amount = int("".join(amount.split(",")))
+
+        inventory = get_object_or_404(Inventory, inventory_type=inventory_type)
+
+        if account_id:
+            account = get_object_or_404(Account, id=account_id)
+
+        if action == "deposit":
+            inventory.deposit(amount, account, description)
+            return Response(
+                {"status": "Deposit successful"}, status=status.HTTP_201_CREATED
+            )
+
+        elif action == "withdraw":
+            try:
+                inventory.withdraw(amount, account, description)
+                return Response(
+                    {"status": "Withdraw successful"}, status=status.HTTP_200_OK
+                )
+            except ValueError:
+                return Response(
+                    {"error": "Insufficient funds"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        return Response({"error": "Invalid action"}, status=status.HTTP_400_BAD_REQUEST)
